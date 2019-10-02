86394c8c682fe3ec7c3d8049ca4684af484ae024
Alex <alexmiddleton@outlook.com>
2019-10-01 23:17:15 -0400

    Cleaning up warning messages.

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,23 +7,13 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 import "../pages/mystyles.scss"
 
-import Header from "./header"
+// import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import "./mystyles.scss"
 
