ca5bd542306ab1dc6b4b398c6833b5a2ea06a698
Alex <alexmiddleton@outlook.com>
2019-10-01 23:29:10 -0400

    Made needless change to attempt to get Netlify to deploy the site

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 
 const About = () => (
     <Layout>
-        <h1 className="has-text-centered">Under construction.  Check back soon!</h1>
+        <h1 className="has-text-centered">Under construction. Check back soon!</h1>
     </Layout>
 )
 
