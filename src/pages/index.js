1697fa92d39f6825ca1173a428cc0a43d898c4cb
Alex <alexmiddleton@outlook.com>
2019-12-18 18:56:32 -0500

    Getting rid of warning messages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,94 +1,94 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
-import Layout from '../components/layout'
-import Navbar from '../components/Navbar/Navbar'
-import Signature from '../components/Hero/Signature'
-import Process from '../components/Process/Process'
+// import { graphql, useStaticQuery } from 'gatsby'
+// import Layout from '../components/layout'
+// import Navbar from '../components/Navbar/Navbar'
+// import Signature from '../components/Hero/Signature'
+// import Process from '../components/Process/Process'
 
 
 
 const Index = (props) => {
-    const data = useStaticQuery(graphql`
-    query MyQuery {
-        allContentfulHomePage {
-          edges {
-            node {
-              myMoneyMaker {
-                fluid(maxWidth: 425) {
-                  src
-                }
-                description
-              }
-              myName
-              myTitle
-              digitalSolutions
-              curatingCompanyCulture
-              howAreTheyConnected
-              strategicOperationsServices
-              contactMe
-              placeHolder {
-                fluid(maxWidth: 425) {
-                  src
-                }
-                description
-              }
-              servicesOfferedHeader {
-                fluid(maxWidth: 920) {
-                  src
-                }
-                description
-              }
-              kristinKeeGraphic {
-                fluid(maxWidth: 425) {
-                  src
-                }
-                description
-              }
-              digitalServicesOfferedImage {
-                description
-                fluid(maxWidth: 425) {
-                  src
-                }
-              }
-              initials {
-                description
-                fluid(maxWidth: 60) {
-                  src
-                }
-              }
-              strategicServicesImage {
-                description
-                fluid(maxWidth: 425) {
-                  src
-                }
-              }
-            }
-          }
-        }
-        contentfulHomePageDescriptionRichTextNode {
-          json
-        }
-        contentfulHomePageUniqueSolutionsDescriptionRichTextNode {
-          json
-        }
-        contentfulHomePageCompanyCultureDescriptionRichTextNode {
-          json
-        }
-        contentfulHomePageConnectedDescriptionRichTextNode {
-          json
-        }
-        contentfulHomePageDescriptionOfServicesOfferedRichTextNode {
-          json
-        }
-        contentfulHomePageDescriptionOfStrategicOperationsServicesRichTextNode {
-          json
-        }
-        contentfulHomePageContactMeInformationRichTextNode {
-          json
-        }
-      }
+    // const data = useStaticQuery(graphql`
+    // query MyQuery {
+    //     allContentfulHomePage {
+    //       edges {
+    //         node {
+    //           myMoneyMaker {
+    //             fluid(maxWidth: 425) {
+    //               src
+    //             }
+    //             description
+    //           }
+    //           myName
+    //           myTitle
+    //           digitalSolutions
+    //           curatingCompanyCulture
+    //           howAreTheyConnected
+    //           strategicOperationsServices
+    //           contactMe
+    //           placeHolder {
+    //             fluid(maxWidth: 425) {
+    //               src
+    //             }
+    //             description
+    //           }
+    //           servicesOfferedHeader {
+    //             fluid(maxWidth: 920) {
+    //               src
+    //             }
+    //             description
+    //           }
+    //           kristinKeeGraphic {
+    //             fluid(maxWidth: 425) {
+    //               src
+    //             }
+    //             description
+    //           }
+    //           digitalServicesOfferedImage {
+    //             description
+    //             fluid(maxWidth: 425) {
+    //               src
+    //             }
+    //           }
+    //           initials {
+    //             description
+    //             fluid(maxWidth: 60) {
+    //               src
+    //             }
+    //           }
+    //           strategicServicesImage {
+    //             description
+    //             fluid(maxWidth: 425) {
+    //               src
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    //     contentfulHomePageDescriptionRichTextNode {
+    //       json
+    //     }
+    //     contentfulHomePageUniqueSolutionsDescriptionRichTextNode {
+    //       json
+    //     }
+    //     contentfulHomePageCompanyCultureDescriptionRichTextNode {
+    //       json
+    //     }
+    //     contentfulHomePageConnectedDescriptionRichTextNode {
+    //       json
+    //     }
+    //     contentfulHomePageDescriptionOfServicesOfferedRichTextNode {
+    //       json
+    //     }
+    //     contentfulHomePageDescriptionOfStrategicOperationsServicesRichTextNode {
+    //       json
+    //     }
+    //     contentfulHomePageContactMeInformationRichTextNode {
+    //       json
+    //     }
+    //   }
       
-    `)
+    // `)
     
     return (
       // <Layout>
