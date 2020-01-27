3ae91720d8671196959a2ef7eafefcc970e8a71c
Alex <alexmiddleton@outlook.com>
2020-01-26 23:41:45 -0500

    Updated Process component to be more mobile friendly.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Layout from '../components/layout'
-import Navbar from '../components/Navbar/Navbar'
 import Signature from '../components/Hero/Signature'
 import Process from '../components/Process/Process'
 import Results from '../components/Results/Results'
@@ -9,7 +8,6 @@ const Index = (props) => {
     return (
       <Layout>
         <div className='container'>
-            <Navbar />
             <div className='hero-container'>
                 { typeof window !== `undefined` ? <Signature /> : null }
                 <h3>Strategic Social Media Marketing</h3>
