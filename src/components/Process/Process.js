273145f41f08f10b3be278f9802d667f0f21842a
Alex <alexmiddleton@outlook.com>
2020-01-31 17:39:46 -0500

    Adjusted formatting of Process section to vertically center better on desktop and mobile.

--- src/components/Process/Process.js
+++ src/components/Process/Process.js
@@ -8,19 +8,19 @@ const Process = (props) => {
             <h2>How it works...</h2>
             <div className='process-list'>
                 <div className='about-item'>
-                    <p><span className='step'>Step 1. </span>We discuss your business goals.</p>
+                    <div><span className='step'>Step 1. </span>We discuss your business goals.</div>
                 </div>
                 <div className='about-item'>
-                    <p><span className='step'>Step 2. </span>We perform a detailed social media audit.</p>
+                    <div><span className='step'>Step 2. </span>We perform a detailed social media audit.</div>
                 </div>
                 <div className='about-item'>
-                    <p><span className='step'>Step 3. </span>We create a social media strategy tailored to your specific company and its goals.</p>
+                    <div><span className='step'>Step 3. </span>We create a social media strategy tailored to your specific company and its goals.</div>
                 </div>
                 <div className='about-item'>
-                    <p><span className='step'>Step 4. </span>We build a content calendar designed to maximize engagement and build a stronger brand.</p>
+                    <div><span className='step'>Step 4. </span>We build a content calendar designed to maximize engagement and build a stronger brand.</div>
                 </div>
                 <div className='about-item'>
-                    <p><span className='step'>Step 5. </span>We monitor the results and optimize the strategy based on performance.</p>
+                    <div><span className='step'>Step 5. </span>We monitor the results and optimize the strategy based on performance.</div>
                 </div>
                 <div className='process-buttons'>
                     <div className='contact-us'>
