873e43949c38e745c8e7356a82867db7c844ddda
Alex <alexmiddleton@outlook.com>
2020-02-23 08:25:05 -0500

    Updating service cards layout on mobile devices.  Fixed key errors on mapped cards.

--- src/components/OtherServices/OtherServicesList.js
+++ src/components/OtherServices/OtherServicesList.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import './OtherServicesList.scss'
-import OtherServiceCard from './OtherServiceCard/OtherServiceCard'
-import OtherServices from './OtherServiceObject'
+import React from "react"
+import { Link } from "gatsby"
+import "./OtherServicesList.scss"
+import OtherServiceCard from "./OtherServiceCard/OtherServiceCard"
+import OtherServices from "./OtherServiceObject"
 
 const OtherServicesList = () => {
-    return (
-        <div className='services-main'>
-            <div className='services-container'>
-                <h2 className='services-header'>Additional Services Offered</h2>
-                <div className='list-container'>
-                    {OtherServices.map(service => <OtherServiceCard title={service.title} description={service.description} /> )}
-                </div>
-                <h4>For more information on custom services, <Link to='/contact'>contact us.</Link></h4>
-            </div>
+  return (
+    <div className="services-main">
+      <div className="services-container">
+        <h2 className="services-header">Additional Services Offered</h2>
+        <div className="list-container">
+          {OtherServices.map(service => (
+            <OtherServiceCard
+              key={service.id}
+              title={service.title}
+              description={service.description}
+            />
+          ))}
         </div>
-    )
+        <h4>
+          For more information on custom services,{" "}
+          <Link to="/contact">contact us.</Link>
+        </h4>
+      </div>
+    </div>
+  )
 }
 
 export default OtherServicesList
